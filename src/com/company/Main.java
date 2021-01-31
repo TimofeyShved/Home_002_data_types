0316d379ce5eb25795bb2d7286b7aeb0c5f49138
timofeya123 <timofeya123@inbox.ru>
2021-01-31 18:45:47 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    byte age = 0;//-128 - 128
+    short veight = 1234; //-32 - 32
+    int i = 231243; // 4 byte
+    long l = 3214124; // 8 byte
+    float f = 12.312f; // 4 byte
+    double d = 123.3213; // 8 byte
+    boolean bool = true; // true/false
+
+    char c ='a';
+    String s = "Help!";
+
+
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        System.out.println(car1.color);
+	// write your code here
+    }
+}
+
