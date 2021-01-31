4998fb4cadc23503806208fa3d01ebfd6118eb6b
timofeya123 <timofeya123@inbox.ru>
2021-01-31 18:52:55 +0600

    class Humen

---
+++ src/com/company/Humen.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Humen {
+    int age = 20;
+
+    double getAge(int i, double d, Humen humen){
+        return humen.age;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
     byte age = 0;//-128 - 128
     short veight = 1234; //-32 - 32
-    int i = 231243; // 4 byte
+    static int i = 231243; // 4 byte
     long l = 3214124; // 8 byte
-    float f = 12.312f; // 4 byte
+    static float f = 12.312f; // 4 byte
     double d = 123.3213; // 8 byte
     boolean bool = true; // true/false
 
@@ -17,6 +17,8 @@ public class Main {
     public static void main(String[] args) {
         Car car1 = new Car();
         System.out.println(car1.color);
+        Humen humen1 = new Humen();
+        System.out.println(humen1.getAge(i,f,humen1));
 	// write your code here
     }
 }
